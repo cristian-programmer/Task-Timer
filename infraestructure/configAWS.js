962772e307ef31c4b41d378f54196355bd6149d0
Cristian <cvg97@misena.edu.co>
2021-02-23 17:17:16 -0500

    FIle config for connection DynamonDB

---
+++ infraestructure/configAWS.js
@@ -0,0 +1,10 @@
+const configAWS = {
+    region: process.env.REGION,
+    endpoint: process.env.END_POINT,
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY
+};
+
+module.exports = {
+    configAWS
+};
