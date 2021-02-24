bf5e95cbac4cf2b38d18dabc318695771862d298
Cristian <cvg97@misena.edu.co>
2021-02-23 22:47:33 -0500

    updated controllers, tables of DynamonDB added

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const  logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const projectController = require('./controllers/project');
+const taskController = require('./controllers/task');
+const timeTracking = require('./controllers/timeTracking');
+const usersController = require('./controllers/users');
 
-var app = express();
+const app = express();
+
+const Infraestructure = require("./infraestructure/dynamondb");
+
+Infraestructure.createTable().then(res => {
+    console.log("Successfull CreateTables AWS DynamonDB ",res);
+}).catch(error => {
+    console.error(error);
+});
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/project', projectController);
+app.use('/task', taskController);
+app.use('/timeTracking', timeTracking);
+app.use('/users', usersController);
+
 
 module.exports = app;
