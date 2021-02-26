0cf6071b8474cda865f32b04ee6d13c84092991d
Cristian <cvg97@misena.edu.co>
2021-02-26 14:54:42 -0500

    updated app.js added new controllers

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const  logger = require('morgan');
 
+const mongo = require("./infraestructure/mongoose");
+mongo.createConnection();
 const projectController = require('./controllers/project');
 const taskController = require('./controllers/task');
 const timeTracking = require('./controllers/timeTracking');
 const usersController = require('./controllers/users');
+
+
 
 const app = express();
 
@@ -14,9 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-/*app.use('/project', projectController);
-app.use('/task', taskController);
-app.use('/timeTracking', timeTracking);*/
+
+/*app.use('/timeTracking', timeTracking);*/
+app.use('/v1', projectController);
+app.use('/v1', taskController);
 app.use('/v1', usersController);
 
 
