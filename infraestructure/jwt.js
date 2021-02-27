60d33ae4ef66fef9f76dfb01ef868ccb1b86f429
Cristian <cvg97@misena.edu.co>
2021-02-27 08:14:43 -0500

    support for env variable for run test

--- infraestructure/jwt.js
+++ infraestructure/jwt.js
@@ -1,13 +1,18 @@
 const jwt = require ('jsonwebtoken');
+const config =  require("config");
 
+const TOKEN  = process.env.NODE_ENV === undefined 
+    ? config.get("TOKEN") :  process.env.TOKEN;
+
+console.log("TOKEN", TOKEN);
 const createSign = (info) => {
-    return jwt.sign(info, process.env.TOKEN, {expiresIn: 1440});
+    return jwt.sign(info, TOKEN, {expiresIn: 1440});
 }
 
 const autorizeRoute = (req, res, next) => {
     const token = req.headers["access-token"];
     if(token) {
-        jwt.verify(token, process.env.TOKEN, (error, decoded) => {
+        jwt.verify(token, TOKEN , (error, decoded) => {
             if(error) {
                 res.sendStatus(403); 
             }else {
--- infraestructure/mongoose.js
+++ infraestructure/mongoose.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
+const config = require("config");
+
+const MONGO_DB_HOST = process.env.NODE_ENV === undefined  ? 
+ config.get("MONGO_DB_HOST") : process.env.MONGO_DB_HOST; 
 
 const createConnection = () => {
-    mongoose.connect("mongodb://172.17.0.2:27017/TaskTimer", {
+    mongoose.connect(MONGO_DB_HOST, {
         useCreateIndex: true,
         useNewUrlParser: true,
         useFindAndModify: false,
