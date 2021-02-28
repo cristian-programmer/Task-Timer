8ec434cd8d7972d28bd7de1d2ae9fb9ae53fb8c6
Cristian <cvg97@misena.edu.co>
2021-02-28 18:06:43 -0500

    support for cors

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ const mongo = require("./infraestructure/mongoose");
 mongo.createConnection();
 const projectController = require('./controllers/project');
 const taskController = require('./controllers/task');
-const timeTracking = require('./controllers/timeTracking');
 const usersController = require('./controllers/users');
 
-
+const cors = (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Authorization, X-API-KEY, Origin, " +
+    "X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, OPTIONS, PUT, DELETE");
+    next();
+}
 
 const app = express();
 
@@ -20,6 +25,7 @@ app.use(cookieParser());
 
 
 /*app.use('/timeTracking', timeTracking);*/
+app.use(cors);
 app.use('/v1', projectController);
 app.use('/v1', taskController);
 app.use('/v1', usersController);
