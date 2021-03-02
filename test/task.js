3435b094efad96d0f79bf09f3f73df2e0dc73ffc
Cristian Vargas <cvg97@misena.edu.co>
2021-03-02 16:26:25 -0500

    Working in unit test and support for test coverage using nyc

--- test/task.js
+++ test/task.js
@@ -1,128 +1,126 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 chai.should();
-const server = require("./../app");
-const taskModel = require("./../models/task");
+const server = require("../app");
+const taskModel = require("../models/task");
 
 chai.use(chaiHttp);
 
-describe("API TASK" , () => {
-    beforeEach((done)=> {
-        taskModel.cleanSchemaTask().then(res=> {
-           done();
-        }).catch(error => {
-            throw error;
-        });
-    });
-    
-    let idUser = "";
-    it("should  logged a user for create a task", (pass) => {
-        const user = {
-            username: "cvg97",
-            password: "abc1"
-        };
+describe("API TASK", () => {
+  before((done) => {
+    taskModel
+      .cleanSchemaTask()
+      .then((res) => {
+        console.log("Task Before");
+        done();
+      })
+      .catch((error) => {
+        throw error;
+      });
+  });
 
+  let idUser = "";
+  it("should  logged a user for create a task", (pass) => {
+    const user = {
+      username: "cvg97",
+      password: "abc1",
+    };
 
-        chai.request(server)
-        .post("/v1/users/login")
-        .send(user)
-        .end((error, res) => {
-            res.should.have.status(200);
-            res.headers["content-type"].should.contains("application/json");
-            res.body.should.have.property("id");
-            res.body.should.have.property("token");
-            res.body.should.have.property("login").eql(true);
-           
-            idUser = res.body.id;
-            pass();
-           
-        });
-    });
+    chai
+      .request(server)
+      .post("/v1/users/login")
+      .send(user)
+      .end((error, res) => {
+        res.should.have.status(200);
+        res.headers["content-type"].should.contains("application/json");
+        res.body.should.have.property("id");
+        res.body.should.have.property("token");
+        res.body.should.have.property("login").eql(true);
 
-    it("should create a task for a user logged 1", (pass) => {
-        console.log("idUser ", idUser);
+        idUser = res.body.id;
+        pass();
+      });
+  });
 
-        const task = {
-            idUser,
-            name: "Task 1",
-            description: "Description 1 test",
-        };
-        chai.request(server)
-        .post("/v1/tasks")
-        .send(task)
-        .end((error, res) => {
-            res.should.have.status(201);
-            res.headers["content-type"].should.contains("application/json");
-            res.body.should.have.property("created").eql("created");
-            pass();
-        });
-    });
+  it("should create a task for a user logged 1", (pass) => {
+    const task = {
+      idUser,
+      name: "Task 1",
+      description: "Description 1 test",
+    };
+    chai
+      .request(server)
+      .post("/v1/tasks")
+      .send(task)
+      .end((error, res) => {
+        res.should.have.status(201);
+        res.headers["content-type"].should.contains("application/json");
+        res.body.should.have.property("created").eql("created");
+        pass();
+      });
+  });
 
+  it("should create a task for a user logged 2", (pass) => {
+    const task = {
+      idUser,
+      name: "Task 2",
+      description: "Description 2 test",
+    };
+    chai
+      .request(server)
+      .post("/v1/tasks")
+      .send(task)
+      .end((error, res) => {
+        res.should.have.status(201);
+        res.headers["content-type"].should.contains("application/json");
+        res.body.should.have.property("created").eql("created");
+        pass();
+      });
+  });
 
-    it("should create a task for a user logged 2", (pass) => {
-        console.log("idUser ", idUser);
+  it("should fail to create a task for a user logged", (pass) => {
+    const task = {
+      idUser,
+      description: "Description task",
+    };
+    chai
+      .request(server)
+      .post("/v1/tasks")
+      .send(task)
+      .end((error, res) => {
+        res.should.have.status(400);
+        res.body.should.be.a("object");
+        res.body.should.property("error").eql("missing send parameters");
 
-        const task = {
-            idUser,
-            name: "Task 2",
-            description: "Description 2 test",
-        };
-        chai.request(server)
-        .post("/v1/tasks")
-        .send(task)
-        .end((error, res) => {
-            res.should.have.status(201);
-            res.headers["content-type"].should.contains("application/json");
-            res.body.should.have.property("created").eql("created");
-            pass();
-        });
-    });
+        pass();
+      });
+  });
 
+  it("should get all tasks for a user logged", (pass) => {
+    chai
+      .request(server)
+      .get("/v1/tasks/user/" + idUser)
+      .end((error, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+        const tasks = res.body.task;
+        tasks.should.be.a("array");
+        pass();
+      });
+  });
 
-    it("should fail to create a task for a user logged", (pass) => {
-        console.log("idUser ", idUser);
+  it("should fail to get all tasks for a user logged", (pass) => {
+    chai
+      .request(server)
+      .get("/v1/tasks/user/")
+      .end((error, res) => {
+        res.should.have.status(404);
+        pass();
+      });
+  });
 
-        const task = {
-            idUser,
-            description: "Description task"
-           
-        };
-        chai.request(server)
-        .post("/v1/tasks")
-        .send(task)
-        .end((error, res) => {
-            res.should.have.status(400);
-            res.body.should.be.a("object");
-            res.body.should.property("error").eql("missing send parameters");
-
-            pass();
-        });
-    });
-
-    it("should get all tasks for a user logged", (pass) => {
-        console.log("idUser ", idUser);
-
-        chai.request(server)
-        .get("/v1/tasks/user/"+idUser)
-        .end((error, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a("object");
-            const tasks = res.body.task;
-            console.log(tasks);
-            tasks.should.be.a("array");
-            tasks.should.have.lengthOf(2);
-
-            pass();
-        });
-    });
-
-    it("should fail to get all tasks for a user logged", (pass) => {
-
-        chai.request(server)
-        .get("/v1/tasks/user/")
-        .end((error, res) => {
-            res.should.have.status(404);
-            pass();
-        });
-    });
+  after((done) => {
+    console.log("Task After");
+    done();
+  });
 });
--- test/users.js
+++ test/users.js
@@ -1,96 +1,102 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 chai.should();
-const server = require("./../app");
-const mongo = require("./../infraestructure/mongoose");
-const userModel = require("./../models/users");
+const server = require("../app");
+const mongo = require("../infraestructure/mongoose");
+const userModel = require("../models/users");
 
 chai.use(chaiHttp);
 
 describe("User API ", () => {
+  before((done) => {
+    userModel
+      .cleanSchemaUser()
+      .then((res) => {
+        console.log("Test user beforeEach");
+        done();
+      })
+      .catch((error) => {
+        throw error;
+      });
+  });
 
-    before((done)=>{
-        userModel.cleanSchemaUser().then(res => {
-            done();
-        }).catch(error => {
-            throw error;
-        });
-    });
+  it("should create a user", (pass) => {
+    const user = {
+      name: "cristian vargas",
+      username: "cvg97",
+      password: "abc1",
+      email: "cvg97@gmail.com",
+    };
 
-    it("should create a user", (pass) => {
-        const user = {
-            name: "cristian vargas",
-            username: "cvg97",
-            password: "abc1",
-            email: "cvg97@gmail.com"
-        };
+    chai
+      .request(server)
+      .post("/v1/users")
+      .send(user)
+      .end((error, res) => {
+        res.should.have.status(201);
+        res.headers["content-type"].should.contains("application/json");
+        pass();
+      });
+  });
 
-        chai.request(server)
-        .post("/v1/users")
-        .send(user)
-        .end((error, res) => {
-            res.should.have.status(201);
-            res.headers["content-type"].should.contains("application/json");
-            pass();
-        });
-    });
+  it("shoud fail to create a user", (pass) => {
+    const user = {
+      username: "cvg97",
+      password: "abc1",
+    };
 
+    chai
+      .request(server)
+      .post("/v1/users")
+      .send(user)
+      .end((error, res) => {
+        res.should.have.status(400);
+        res.headers["content-type"].should.contains("application/json");
+        res.body.should.have.property("error").eql("missing send parameters");
+        pass();
+      });
+  });
 
-    it("shoud fail to create a user", (pass) => {
-        const user = {
-            username: "cvg97",
-            password: "abc1"
-        };
+  it("should logged a user", (pass) => {
+    const user = {
+      username: "cvg97",
+      password: "abc1",
+    };
 
-        chai.request(server)
-        .post("/v1/users")
-        .send(user)
-        .end((error, res) => {
-            res.should.have.status(400);
-            res.headers["content-type"].should.contains("application/json");
-            res.body.should.have.property("error").eql("missing send parameters");
-            pass();
-        });
-    });
+    chai
+      .request(server)
+      .post("/v1/users/login")
+      .send(user)
+      .end((error, res) => {
+        res.should.have.status(200);
+        res.headers["content-type"].should.contains("application/json");
+        res.body.should.have.property("id");
+        res.body.should.have.property("token");
+        res.body.should.have.property("login").eql(true);
+        pass();
+      });
+  });
 
-    it("should logged a user", (pass) => {
-        const user = {
-            username: "cvg97",
-            password: "abc1"
-        };
+  it("should fail logged a user", (pass) => {
+    const user = {
+      username: "cvg97",
+    };
 
-        chai.request(server)
-        .post("/v1/users/login")
-        .send(user)
-        .end((error, res) => {
-            res.should.have.status(200);
-            res.headers["content-type"].should.contains("application/json");
-            res.body.should.have.property("id");
-            res.body.should.have.property("token");
-            res.body.should.have.property("login").eql(true);
-            pass();
-        });
-    });
+    chai
+      .request(server)
+      .post("/v1/users/login")
+      .send(user)
+      .end((error, res) => {
+        res.should.have.status(400);
+        res.headers["content-type"].should.contains("application/json");
+        res.body.should.have.property("error").eql("missing send parameters");
+        res.body.should.have.property("login").eql(false);
+        pass();
+      });
+  });
 
-
-    it("should fail logged a user", (pass) => {
-        const user = {
-            username: "cvg97",
-        };
-
-        chai.request(server)
-        .post("/v1/users/login")
-        .send(user)
-        .end((error, res) => {
-            res.should.have.status(400);
-            res.headers["content-type"].should.contains("application/json");
-            res.body.should.have.property("error").eql("missing send parameters");
-            res.body.should.have.property("login").eql(false);
-            pass();
-        });
-    });
-
-    after(()=> {
-        mongo.closeConnection();
-    })
+  after(() => {
+    console.log("Test user after");
+    mongo.closeConnection();
+  });
 });
