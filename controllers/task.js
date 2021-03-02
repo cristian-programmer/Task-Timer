da03f955826d9533f0799c21da3fbea9909ccdd9
Cristian Vargas <cvg97@misena.edu.co>
2021-03-02 01:15:38 -0500

    addTimeTracking function added

--- controllers/task.js
+++ controllers/task.js
@@ -1,45 +1,52 @@
-const express =  require('express');
+const express = require("express");
 const router = express.Router();
 
-const taskModel = require('../models/task');
+const taskModel = require("../models/task");
 
 router.get("/tasks/user/:id", async (req, res) => {
-    const idUser = req.params.id;
-    try {
-        if(idUser){
-            const task = await taskModel.getAllTaskByUser({idUser});
-            return res.status(200).json({task});
-        }
-        res.status(400).json({error:"missing send parameters"});
-
-    } catch (error) {
-        console.error(error);
-        res.sendStatus(500);
+  const idUser = req.params.id;
+  try {
+    if (idUser) {
+      const task = await taskModel.getAllTaskByUser({ idUser });
+      return res.status(200).json({ task });
     }
+    res.status(400).json({ error: "missing send parameters" });
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 });
 
 router.patch("/tasks/:id", async (req, res) => {
-   const idTask = req.params.id;
-   res.json({idTask});  
+  const idTask = req.params.id;
+  const time = req.body;
+  try {
+    if (idTask && time) {
+      const updated = taskModel.addTimeTracking(idTask, time);
+      return res.status(201).json({ updated });
+    }
+    res.status(400).json({ error: "missing send parameters" });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
 });
 
 router.post("/tasks", async (req, res) => {
-   const task = req.body;
-   const {idUser, name, description} = task;
-   try {
-    if(idUser && name && description){
-        const saved = await taskModel.createTask(task);
-        console.log(saved);
-        if(Object.keys(saved).length > 0) 
-            return res.status(201).json({created: "created"});
-        
+  const task = req.body;
+  const { idUser, name, description } = task;
+  try {
+    if (idUser && name && description) {
+      const saved = await taskModel.createTask(task);
+      console.log(saved);
+      if (Object.keys(saved).length > 0)
+        return res.status(201).json({ created: "created" });
     }
-    res.status(400).json({error: "missing send parameters"});   
-   } catch (error) {
-       console.error(error);
-       res.sendStatus(500);
-   }
+    res.status(400).json({ error: "missing send parameters" });
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 });
 
-
-module.exports =  router;
+module.exports = router;
--- models/task.js
+++ models/task.js
@@ -1,50 +1,57 @@
 const mongoose = require("mongoose");
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
 const TaskSchema = new Schema({
-    idUser: {type: String, required: true},
-    name: {type: String, required: true},
-    description: {type: String},
-    timeTracking: {
-        startDate: { type: Date},
-        endingDate: {type: Date} 
-    }
+  idUser: { type: String, required: true },
+  name: { type: String, required: true },
+  description: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  timeTracking: {
+    hours: { type: Number },
+    minutes: { type: Number },
+    seconds: { type: Number },
+  },
 });
 
 const taskModel = mongoose.model("task", TaskSchema);
 const createTask = async (task) => {
-    try {
-        
-        const taskItem = new taskModel(task);
-        return await taskItem.save(task);
-
-    } catch (error) {
-        return error;
-    }
-}
+  try {
+    const taskItem = new taskModel(task);
+    return await taskItem.save(task);
+  } catch (error) {
+    return error;
+  }
+};
 
 const getAllTaskByUser = async (idUser) => {
-    console.log("getAllTaskByUser ",idUser)
-    return await taskModel.find(idUser);
-}
+  console.log("getAllTaskByUser ", idUser);
+  return await taskModel.find(idUser);
+};
 
 const getAllTask = async () => {
-    return await taskModel.find();
-}
+  return await taskModel.find();
+};
 
+const addTimeTracking = async (idTask, time) => {
+  try {
+    return await taskModel.updateOne({ _id: idTask }, { time });
+  } catch (error) {
+    return error;
+  }
+};
 
-const cleanSchemaTask = async() => {
-    try {
-        return await userModel.deleteMany({});
-    } catch (error) {
-        return error;
-    }
-    
-}
+const cleanSchemaTask = async () => {
+  try {
+    return await userModel.deleteMany({});
+  } catch (error) {
+    return error;
+  }
+};
 
 module.exports = {
-    createTask,
-    getAllTaskByUser,
-    getAllTask,
-    cleanSchemaTask
-}
+  createTask,
+  getAllTaskByUser,
+  getAllTask,
+  addTimeTracking,
+  cleanSchemaTask,
+};
