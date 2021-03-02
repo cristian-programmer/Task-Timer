7d7f7dc853f59a6ca07ee2755b7d9bab59c643fd
Cristian Vargas <cvg97@misena.edu.co>
2021-03-02 01:19:01 -0500

    added console.log() for test propurse

--- infraestructure/mongoose.js
+++ infraestructure/mongoose.js
@@ -1,30 +1,32 @@
 const mongoose = require("mongoose");
 const config = require("config");
 
-const MONGO_DB_HOST = process.env.NODE_ENV === undefined  ? 
- config.get("MONGO_DB_HOST") : process.env.MONGO_DB_HOST; 
-
+const MONGO_DB_HOST =
+  process.env.NODE_ENV === undefined
+    ? config.get("MONGO_DB_HOST")
+    : process.env.MONGO_DB_HOST;
+console.log("Connection " + MONGO_DB_HOST);
 const createConnection = () => {
-    mongoose.connect(MONGO_DB_HOST, {
-        useCreateIndex: true,
-        useNewUrlParser: true,
-        useFindAndModify: false,
-        useUnifiedTopology:true
-    }).then(db => {
-        console.info("DB connect");
-    }).catch(error => {
-        console.error(error);
+  mongoose
+    .connect(MONGO_DB_HOST, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    })
+    .then((db) => {
+      console.info("DB connect");
+    })
+    .catch((error) => {
+      console.error(error);
     });
-}
+};
 
 const closeConnection = () => {
-    mongoose.connection.close();
-}
+  mongoose.connection.close();
+};
 
 module.exports = {
-    createConnection,
-    closeConnection
-}
-
-
-
+  createConnection,
+  closeConnection,
+};
